97dbffe549c1deee997a0a5af9adc1182ebb6c5f
akshatchavann <akshatchavan2022@gmail.com>
2023-11-01 10:59:44 -0400

    basic styles

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -7,7 +7,7 @@ import Header from '../components/Header'
 
 const Home = () => {
     return (
-        <div>
+        <div className="homecontainter">
             <div className="description">
             <Link className='nav-link' to="/">
                 <img src={logo} alt="Website Logo" />
@@ -22,12 +22,12 @@ const Home = () => {
             </div>
             <div className="buttonContainer">
                     <Link to="/UserSignup">
-                        <button type="button" className="signup_button">
+                        <button type="button" className="home_signup_button">
                             Sign up
                         </button>
                     </Link>
                     <Link to="/UserLogin">
-                        <button type="button" className="signup_button">
+                        <button type="button" className="home_signup_button">
                             Login 
                         </button>
                     </Link>
