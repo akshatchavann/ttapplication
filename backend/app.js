c73f72bff022adf5db90c84651264276be31d2e1
akshatchavann <akshatchavan2022@gmail.com>
2023-11-01 12:05:35 -0400

    backend functionality on dummy data

---
+++ backend/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const usersRoutes = require('./routes/users-routes');
+const HttpError = require('./models/http-error');
+
+const app = express();
+
+//bodyParser is a middleware that parses the body of the request
+app.use(bodyParser.json());
+
+app.use('/api/users', usersRoutes);
+
+//in case of error
+app.use((req, res, next) => {
+    const error = new HttpError('Could not find this route.', 404);
+    throw error;
+  });
+  
+app.use((error, req, res, next) => {
+    if (res.headerSent) {
+        return next(error);
+    }   
+    res.status(error.code || 500)
+    res.json({message: error.message || 'An unknown error occurred!'});
+});
+
+app.listen(3000);
