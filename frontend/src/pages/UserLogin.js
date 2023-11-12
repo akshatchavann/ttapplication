27b1fc99ba4daff457fe1cb3d5deeff978d100cc
Akshat Chavan Patil <121521773+akshatchavann@users.noreply.github.com>
2023-11-12 11:08:21 -0500

    Update UserLogin.js

--- frontend/src/pages/UserLogin.js
+++ frontend/src/pages/UserLogin.js
@@ -23,7 +23,7 @@ const UserLogin = () => {
         try {
             e.preventDefault()
             console.log(userdata)
-            const response = await fetch('http://localhost:3000/api/users/login', {
+            const response = await fetch('https://ttapplication-backend.vercel.app/api/users/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
