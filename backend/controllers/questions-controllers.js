b5074cd16660c1b943423916829ea225f44262fa
akshatchavann <akshatchavan2022@gmail.com>
2023-11-21 12:43:09 -0500

    change 2

--- backend/controllers/questions-controllers.js
+++ backend/controllers/questions-controllers.js
@@ -20,10 +20,10 @@ const createQuestion = async (req, res, next) => {
       );
     }
   
-    const { question, bio, category, tweetURL, right, mid, left } = req.body; // Assuming your request body contains 'question' and 'category'
+    const { question, bio, category, tweetboolean, tweetURL, contentboolean, contentURL, right, mid, left } = req.body; // Assuming your request body contains 'question' and 'category'
 
     try {
-        const newQuestion = new Question({ question, bio, category, tweetURL, right, mid, left });
+        const newQuestion = new Question({ question, bio, category, tweetboolean, tweetURL, contentboolean, contentURL, right, mid, left });
         const savedQuestion = await newQuestion.save();
 
         res.status(201).json(savedQuestion); // Send the created question as JSON response with a 201 status code
