1dd78984ea397aadc0cd4b9b03204b985c5fc2a2
akshatchavann <akshatchavan2022@gmail.com>
2023-11-22 22:55:37 -0500

    bugs

--- frontend/src/pages/CompletedPage.js
+++ frontend/src/pages/CompletedPage.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { useParams } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import Header from '../components/Header'
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import "../styles/CompletedPage.css";
 
-
 const CompletedPage = () => {
-    return (
-        <div className="maintext"> Thank you for completing all of the questions!</div>
-    )
-}
+    const { email } = useParams(); // Retrieve the 'email' parameter from the URL
 
+    return (
+        <div className="completedpage">
+            <div className="maintext"> Check back soon for fresh questions that reflect the news of the day! </div>
+            <div style={{ height: '10px' }}></div>
+            <div className="maintext"> In the meantime, click <Link className='prof' to={`/Profile/${email}`}>Profile</Link> to see your answers to previous questions. </div>
+        </div>
+    );
+}
 
 export default CompletedPage;
