c164de32f6799c297df2e7624379345be3c88678
akshatchavann <akshatchavan2022@gmail.com>
2023-11-24 15:45:08 -0500

    Add createRecc function to reccs-controllers.js

--- backend/controllers/reccs-controllers.js
+++ backend/controllers/reccs-controllers.js
@@ -6,5 +6,25 @@ const getReccs = async (req, res, next) => {
     res.send('Hello World!')
 }
 
+const createRecc = async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return next(
+        new HttpError('Invalid inputs passed, please check your data.', 422)
+      );
+    }
+  
+    const { topic, questions, links } = req.body;
+
+    try {
+        const newRecc = new Recc({ topic, questions, links });
+        const savedRecc = await newRecc.save();
+
+        res.status(201).json(savedRecc); 
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Failed to create the reccomendation' });
+    }
+};
 
 exports.getReccs = getReccs;
