f26d712da36767af16e30f199970c2620999d30b
akshatchavann <akshatchavan2022@gmail.com>
2023-12-27 16:13:05 -0500

    style changes

--- frontend/src/pages/TwoOptions.js
+++ frontend/src/pages/TwoOptions.js
@@ -10,18 +10,24 @@ const TwoOptions = () => {
     return (
         <div>
             <div className="yo"> Opinion Captured! </div>
-            <div className="yoo"> Your opinions are safe and secure. We will never sell your data. </div>
+            <div className="yoo"> Your opinions are secure and for your eyes only. We will never sell your data. </div>
             <div className="buttonContainer">
                 <Link to={`/Content/${email}`}>
                     <button type="button" className="home_signup_button">
-                        More Topics 
+                        Explore More Topics 
                     </button>
                 </Link>
                 <Link to={`/Profile/${email}`}>
-                    <button type="button" className="home_signup_button">
+                    <button type="button" className="home_signup_button1">
                         See Profile
                     </button>
                 </Link>
+                <Link to={`/DailyQuestion/${email}`}>
+                    <button type="button">
+                        Go back to Question of the day
+                    </button>
+                </Link>
+
             </div>
         </div>
     )
